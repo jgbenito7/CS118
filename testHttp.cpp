823e0aa5bcb1cba28c97036937db287117fbd26f
Joey Gomez-Benito <jgbenito7@gmail.com>
2016-04-24 15:18:35 -0700

    testing http

---
+++ testHttp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+
+#include "http.h"
+
+using namespace std;
+
+int main(){
+  HttpRequest request;
+  request.setUrl("www.test.com/test");
+  //
+  request.setHeader("Accept","text/html,application/xhtml+xml");
+  request.setHeader("Accept-Language","en-us,en;q=0.5");
+  ByteBlob b = request.encode();
+  HttpRequest decoded = request.decode(b);
+  cout << "First Line: " << decoded.getMethod() << " " << decoded.getUrl() << " HTTP/" << decoded.getVersion() << endl;
+  cout << "Header: " << decoded.getHeader("Accept") << endl;
+}
